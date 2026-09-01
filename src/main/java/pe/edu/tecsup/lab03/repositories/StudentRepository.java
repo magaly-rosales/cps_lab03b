310412851817b55b4caabdfd6f528e8a2958e6cf
Alexis <alexis.prieto@tecsup.edu.pe>
2026-09-01 17:24:39 -0500

    modificacion del StudentRepository

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,4 +1,8 @@
 package pe.edu.tecsup.lab03.repositories;
 
+import pe.edu.tecsup.lab03.models.Student;
+import java.util.List;
+
 public class StudentRepository {
+    public List<Student> findAll() { return null; }
 }
