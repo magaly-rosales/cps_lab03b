50410ab3a06a1352f6b80773febc1845a8c2635a
Alexis <alexis.prieto@tecsup.edu.pe>
2026-09-01 16:43:48 -0500

    Agregación del StudentService

---
+++ src/test/java/pe/edu/tecsup/lab03/services/StudentService.java
@@ -0,0 +1,8 @@
+package pe.edu.tecsup.lab03.services;
+
+public class StudentService {
+    public StudentService() {}
+    public String greet() {
+        return "Hola desde StudentService";
+    }
+}
